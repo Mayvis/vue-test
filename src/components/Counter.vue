e341455f4d3ce8e8a8bb149235fbfd38bc6c1bb9
= <=>
2019-02-20 14:53:17 +0800

    Setting Data and Asserting Styles

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <span class="count" v-text="count"></span>
-    <button @click="count++">Increment</button>
+    <button @click="count++" class="increment">Increment</button>
+    <button @click="count--" v-show="count > 0" class="decrement">Decrement</button>
   </div>
 </template>
 
--- test/counter.spec.js
+++ test/counter.spec.js
@@ -14,10 +14,34 @@ describe('Counter', () => {
         expect(wrapper.vm.count).toBe(0);
     });
 
-    it('increments the count when the button is clicked', () => {
+    it('increments the count when the increment button is clicked', () => {
         expect(wrapper.vm.count).toBe(0);
 
-        wrapper.find('button').trigger('click');
+        wrapper.find('.increment').trigger('click');
+
+        expect(wrapper.vm.count).toBe(1);
+    });
+
+    it('decrements the count when the decrement button is clicked', () => {
+        expect(wrapper.vm.count).toBe(0);
+
+        wrapper.setData({
+            count: 5
+        });
+
+        wrapper.find('.decrement').trigger('click');
+
+        expect(wrapper.vm.count).toBe(4);
+    });
+
+    it('never goes below 0', () => {
+        expect(wrapper.vm.count).toBe(0);
+
+        expect(wrapper.find('.decrement').isVisible()).toBe(false);
+
+        wrapper.setData({count: 1});
+
+        expect(wrapper.find('.decrement').isVisible()).toBe(true);
 
         expect(wrapper.vm.count).toBe(1);
     });
